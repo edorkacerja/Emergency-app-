f0306557ae91827fe678290919002f07f8ba1d91
Edor Kacerja <edorkacerja@gmail.com>
2016-03-18 16:11:24 +0200

    added custom listview adapter

--- app/src/main/java/com/example/acerpc/emergencyapp/Report.java
+++ app/src/main/java/com/example/acerpc/emergencyapp/Report.java
@@ -1,16 +1,26 @@
 package com.example.acerpc.emergencyapp;
 
+import android.content.Context;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class Report extends AppCompatActivity {
 
     ListView listView;
-    String[] data = {"Threat", "Weapon", "Robbery"};
+    String[] reportTitles;
+    int[] icons = {R.drawable.threat, R.drawable.weapon, R.drawable.robbery};
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,12 +33,50 @@ public class Report extends AppCompatActivity {
 
 
         listView = (ListView) findViewById(R.id.listView);
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, data);
-        listView.setAdapter(dataAdapter);
+
+
+        Resources res = getResources();
+        reportTitles = res.getStringArray(R.array.titles);
+
+        EdorsAdapter adapter = new EdorsAdapter(this, reportTitles, icons);
+        listView.setAdapter(adapter);
 
     }
 
 
+    class EdorsAdapter extends ArrayAdapter<String> {
+
+        Context context;
+        int row;
+        int[] imageIcons;
+        String[] myTitles;
+
+        EdorsAdapter(Context c, String[] titles, int[] imgs) {
+            super(c, R.layout.single_row, R.id.textView, titles);
+            this.context = c;
+            this.row = R.layout.single_row;
+            this.imageIcons = imgs;
+            this.myTitles = titles;
+
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            View rowObject = inflater.inflate(row, parent, false);
+
+            ImageView myImage = (ImageView) rowObject.findViewById(R.id.imageView);
+            TextView myText = (TextView) rowObject.findViewById(R.id.textView);
+
+
+            myImage.setImageResource(imageIcons[position]);
+            myText.setText(myTitles[position]);
+
+
+            return rowObject;
+        }
+    }
 
 
     private void initToolbars() {
@@ -49,4 +97,6 @@ public class Report extends AppCompatActivity {
         // Inflate a menu to be displayed in the toolbar
         bottomToolbar.inflateMenu(R.menu.menu_main);
     }
+
+
 }
