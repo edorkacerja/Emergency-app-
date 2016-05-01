1ec6799a49391f810bdeb7121f1ffa3f017f053e
Edor Kacerja <edorkacerja@gmail.com>
2016-05-02 00:32:02 +0300

    making the app more generic

--- app/src/main/java/com/example/acerpc/emergencyapp/Info.java
+++ app/src/main/java/com/example/acerpc/emergencyapp/Info.java
@@ -23,34 +23,5 @@ public class Info extends AppCompatActivity {
 
 
 
-    private void initToolbars() {
-        Toolbar topToolbar = (Toolbar) findViewById(R.id.toolbar_top);
-//        setSupportActionBar(topToolbar);
 
-        Toolbar bottomToolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
-        bottomToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()) {
-                    case R.id.action_call:
-
-                        Intent intent = new Intent(getBaseContext(), EmergencyContact.class);
-                        startActivity(intent);
-                        break;
-                    // TODO: Other cases
-                    case R.id.action_info:
-//                        Intent intent2 = new Intent(getBaseContext(), Info.class);
-//                        startActivity(intent2);
-                        break;
-                    case R.id.action_map:
-                        Intent intent3 = new Intent(getBaseContext(), CampusMap.class);
-                        startActivity(intent3);
-                        break;
-                }
-                return true;
-            }
-        });
-        // Inflate a menu to be displayed in the toolbar
-        bottomToolbar.inflateMenu(R.menu.menu_main);
-    }
 }
