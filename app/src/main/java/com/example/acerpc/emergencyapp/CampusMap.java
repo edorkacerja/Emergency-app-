6270e5c0e9f766b503134fd7386c9b02a794db13
Edor Kacerja <edorkacerja@gmail.com>
2016-05-02 16:29:56 +0300

    few deletions

--- app/src/main/java/com/example/acerpc/emergencyapp/CampusMap.java
+++ app/src/main/java/com/example/acerpc/emergencyapp/CampusMap.java
@@ -4,12 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
 
 
 public class CampusMap extends AppCompatActivity {
-
-    BottomToolbar b = new BottomToolbar();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +17,7 @@ public class CampusMap extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        b.initToolbars();
+        initToolbars();
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -39,5 +38,34 @@ public class CampusMap extends AppCompatActivity {
         return true;
     }
 
+    private void initToolbars() {
+        Toolbar topToolbar = (Toolbar) findViewById(R.id.toolbar_top);
+//        setSupportActionBar(topToolbar);
 
+        Toolbar bottomToolbar = (Toolbar) findViewById(R.id.toolbar_bottom);
+        bottomToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.action_call:
+
+                        Intent intent = new Intent(getBaseContext(), EmergencyContact.class);
+                        startActivity(intent);
+                        break;
+                    // TODO: Other cases
+                    case R.id.action_info:
+                        Intent intent2 = new Intent(getBaseContext(), Info.class);
+                        startActivity(intent2);
+                        break;
+                    case R.id.action_map:
+//                        Intent intent3 = new Intent(getBaseContext(), CampusMap.class);
+//                        startActivity(intent3);
+                        break;
+                }
+                return true;
+            }
+        });
+        // Inflate a menu to be displayed in the toolbar
+        bottomToolbar.inflateMenu(R.menu.menu_main);
+    }
 }
